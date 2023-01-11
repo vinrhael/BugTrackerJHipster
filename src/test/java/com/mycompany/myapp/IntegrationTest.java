bce36de4f7807f431401eba528251f9d057dc6f6
Vinrhael <vinrhael@gmail.com>
2023-01-11 18:35:23 +0000

    Initial version of BugTrackerJHipster generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/mycompany/myapp/IntegrationTest.java
@@ -0,0 +1,31 @@
+package com.mycompany.myapp;
+
+import com.mycompany.myapp.BugTrackerJHipsterApp;
+import com.mycompany.myapp.config.AsyncSyncConfiguration;
+import com.mycompany.myapp.config.EmbeddedElasticsearch;
+import com.mycompany.myapp.config.EmbeddedKafka;
+import com.mycompany.myapp.config.EmbeddedSQL;
+import com.mycompany.myapp.config.TestSecurityConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { BugTrackerJHipsterApp.class, AsyncSyncConfiguration.class, TestSecurityConfiguration.class })
+@EmbeddedElasticsearch
+@EmbeddedKafka
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+    // 5s is the spring default https://github.com/spring-projects/spring-framework/blob/29185a3d28fa5e9c1b4821ffe519ef6f56b51962/spring-test/src/main/java/org/springframework/test/web/reactive/server/DefaultWebTestClient.java#L106
+    String DEFAULT_TIMEOUT = "PT5S";
+
+    String DEFAULT_ENTITY_TIMEOUT = "PT5S";
+}
